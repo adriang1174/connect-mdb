1a6160b8c55da0ef7e5fce32b756ef70680b1c11
Adrian Garcia <adriang_1174@hotmail.com>
2017-09-02 19:49:30 -0300

    adding missing constant

--- config.php
+++ config.php
@@ -2,5 +2,9 @@
 
         define( 'DB_NAME'                   , '/Users/adriang/Documents/0012017.MDB');
         define( 'DB_TYPE'                   , 'PDO');
+        define( 'DB_HOST'                   , '');
+        define( 'DB_USER'                   , '');
+        define( 'DB_PASS'                   , '');
+        define( 'DB_BASE'                   , '');
 
 ?>
